258d48e414255b99d1543c4c71511bbd3c4b9eb6
Jared <jbeckersalax9@aol.com>
2022-07-20 16:22:46 -0400

    commit tree 2062c303699b297b70fd23121a8735220b306415

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,17 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::InvalidRecord => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    # byebug
+    toy = find_toy
+    toy.update!(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -26,6 +30,10 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def find_toy
+    Toy.find_by(id: params[:id])
   end
 
 end
